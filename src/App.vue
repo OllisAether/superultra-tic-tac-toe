d4a1cb990ade380ecb6b6b0d26178b793bcc4edf
Olli <info@ollisaether.art>
2025-04-07 13:20:13 +0200

    style: Update background styles in main.scss for improved visual design; comment out unused Vuetify components in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <VApp>
+  <!-- <VApp>
     <VMain>
-      <VContainer>
-        <RouterView />
-      </VContainer>
+      <VContainer> -->
+  <RouterView />
+      <!-- </VContainer>
     </VMain>
-  </VApp>
+  </VApp> -->
 </template>
